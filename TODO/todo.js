47d2db8a51304271d95706cdea29f86d40e4b6c8
Bryan Chang <keunyoung.chang@gmail.com>
2013-04-30 03:02:16 -0700

    Client-side To-do list app, using jQuery; first commit

--- TODO/todo.js
+++ TODO/todo.js
@@ -1,11 +1,29 @@
-function addTodoList(obj){
-	var list = "<li> " + obj + " </li>";
-	$("#todoList").append(list);
-}
+$(document).ready(function(){
 
-$("button").on('click', function(){
-	var todoInput = $("#todoInput").val();
-	addTodoList(todoInput);
+	$("button").on('click', function(){
+		var todoInput = $("#todo_input").val();
+		var list = "<li>" 
+							+ todoInput
+							+ "<img class='trash' src='trash.svg'>" 
+							+ "</li>";
+		$(list).insertAfter('#todo_list');
+		$("#todo_input").val('');
+
+		$('.trash').on('click', function() {
+			$(this).parent().slideUp(), function(){$(this).remove();}
+		});			
+
+		$('.checkbox').on('click', function() {
+			if ($(this).prop('checked')) {
+					$(this).parent().insertAfter('#completed_list');
+					$(this).parent().css('color', 'red');
+					$(this).parent().css('text-decoration', 'line-through');
+			} 
+			else {
+					$(this).parent().insertAfter('#todo_items');
+					$(this).parent().css('color', 'black');
+					$(this).parent().css('text-decoration', 'none');
+			}
+		});
+	});
 });
-
-
